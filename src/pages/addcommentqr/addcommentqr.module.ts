53683d3a4a9bcbd812c40ded85127d1075d1781e
Alison Rarchaert <alison532@live.fr>
2018-05-16 14:43:07 +0200

    addpages

---
+++ src/pages/addcommentqr/addcommentqr.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AddcommentqrPage } from './addcommentqr';
+
+@NgModule({
+  declarations: [
+    AddcommentqrPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AddcommentqrPage),
+  ],
+})
+export class AddcommentqrPageModule {}
